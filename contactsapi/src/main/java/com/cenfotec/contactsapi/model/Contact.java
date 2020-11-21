2273e0190a03e446f8d41b7e7c998c690cf833b4
Miguel Gutierrez <mgutierrezb@ucenfotec.ac.cr>
2020-11-21 14:57:06 -0600

    Final 1

---
+++ contactsapi/src/main/java/com/cenfotec/contactsapi/model/Contact.java
@@ -0,0 +1,23 @@
+package com.cenfotec.contactsapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+}
+
